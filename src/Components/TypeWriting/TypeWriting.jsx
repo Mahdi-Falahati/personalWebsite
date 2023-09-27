2cc303c5fa9403f629161ddf44c71aa161ea8c0b
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-09-27 14:51:11 -0700

    add icon

--- src/Components/TypeWriting/TypeWriting.jsx
+++ src/Components/TypeWriting/TypeWriting.jsx
@@ -1,16 +1,19 @@
 import styles from "./typeWriting.module.css";
+import { GiDelighted } from "react-icons/gi";
 
 export default function TypeWriting({ text }) {
   return (
     <p
       className={
-        styles.typewriter + " py-2 border-y border-dotted border-gray-500"
+        styles.typewriter + " p-2 border-y border-dotted border-gray-500"
       }
     >
       {text}
       {!text && (
         <>
-          I AM <span className="text-red-700">Mahdi-Falahati (RC)</span>
+          I am
+          <GiDelighted className="mx-1 inline text-red-600" />
+          <span className="text-red-900">Mahdi-Falahati (RC)</span>
         </>
       )}
     </p>
