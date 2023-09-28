e35f3c8cd8e6ecb5f231d0fee5f123b94b163f03
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-09-28 06:14:24 -0700

    use card anim and scroll animation

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,3 +1,6 @@
+import CardAnim from "./CardAnimation/CardAnim";
+import RC from "../Assets/RC.jpg";
+
 export default function AboutMe() {
   return (
     <section className="mt-3 pt-1 h-screen">
@@ -8,6 +11,29 @@ export default function AboutMe() {
         </h1>
         <p className="bg-cyan-500 rounded w-5/6 sm:w-1/3 h-1 "></p>
       </div>
+      <section className="mt-8 h-full flex flex-wrap justify-around align-center">
+        <div
+          data-aos="fade-right"
+          data-aos-offset="300"
+          data-aos-easing="ease-in-sine"
+        >
+          <CardAnim
+            title="My hobbies are...."
+            habbies={habbies}
+            img={{
+              alt: "RC",
+              title: "RC",
+              src: RC,
+            }}
+          />
+        </div>
+      </section>
     </section>
   );
 }
+
+const habbies = [
+  "listening to music",
+  "playing chess",
+  "Writing code & Reading the source code",
+];
