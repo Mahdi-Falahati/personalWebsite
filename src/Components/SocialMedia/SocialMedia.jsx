30df097f86d8ffbfb5c71dfe2e0aec093c681747
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-09-30 06:45:14 -0700

    add title props in the bottom icon

--- src/Components/SocialMedia/SocialMedia.jsx
+++ src/Components/SocialMedia/SocialMedia.jsx
@@ -1,10 +1,11 @@
-export default function SocialMedia({ icon, link, height }) {
+export default function SocialMedia({ icon, link, height, title }) {
   return (
     <a href={link}>
       <img
         src={icon}
         className={`${height || "h-12"} hover:animate-bounce animate-pulse`}
       />
+      <span className="text-gray-400 text-sm font-bold">{title}</span>
     </a>
   );
 }
