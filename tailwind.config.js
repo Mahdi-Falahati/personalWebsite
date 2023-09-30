99090fb04815faf57544985ed405fd0178c61b72
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-09-30 03:43:35 -0700

    change animation name

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         auto: "auto",
       },
       animation: {
-        'spin-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       }
     },
   },
