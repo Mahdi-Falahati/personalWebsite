2ce2bbd7826e8db7ee2f745d006e2219bf3ca5ef
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-09-29 23:48:47 -0700

    add slider for skills

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,53 @@
+import { GiBlackHoleBolas } from "react-icons/gi";
+import Slider from "./Slider/Slider";
+
+import css from "../Assets/Icons/css.svg";
+import html from "../Assets/Icons/html.svg";
+import figma from "../Assets/Icons/figma.svg";
+import jest from "../Assets/Icons/jest.svg";
+import js from "../Assets/Icons/js.svg";
+import mongodb from "../Assets/Icons/mongodb.svg";
+import nextjs from "../Assets/Icons/nextjs.svg";
+import nodejs from "../Assets/Icons/nodejs.svg";
+import python from "../Assets/Icons/python.svg";
+import reactRouterDom from "../Assets/Icons/react-router-dom.svg";
+import reactjs from "../Assets/Icons/reactjs.svg";
+import tailwindcss from "../Assets/Icons/tailwindcss.svg";
+import redux from "../Assets/Icons/redux.svg";
+import git from "../Assets/Icons/git.svg";
+
+export default function Skills() {
+  return (
+    <div className="bg-purple-900 py-10">
+      <section className="flex justify-center items-center border-b border-white pb-3">
+        <h2 className="text-center text-white text-3xl font-bold">SKILLS</h2>
+        <GiBlackHoleBolas className="ml-5 text-white text-3xl" />
+      </section>
+      <section>
+        <div
+          className="py-5 bg-white my-2"
+          data-aos="zoom-in-up"
+        >
+          <Slider images={src} height="h-14" />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+const src = [
+  html,
+  css,
+  tailwindcss,
+  js,
+  reactjs,
+  redux,
+  reactRouterDom,
+  jest,
+  nodejs,
+  nextjs,
+  mongodb,
+  python,
+  figma,
+  git,
+];
